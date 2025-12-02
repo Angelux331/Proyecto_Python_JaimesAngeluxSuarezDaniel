e075e843de670db3fe864d291cd6da60e5755055
Angelux331 <188609074+Angelux331@users.noreply.github.com>
2025-12-02 12:29:29 -0500

    Delete utils/menuscontenido.py

--- utils/menuscontenido.py
+++
@@ -1,4 +0,0 @@
-menuPrincipal = {'1': 'Registrar gasto', '2': 'Listar gastos', '3': 'Calcular total de gastos', '4': 'Generar reporte de gastos', '5': 'Salir'}
-menuListarGastos = {'1': 'Ver todos los gastos', '2': 'Filtrar por categoría', '3': 'Filtrar por rango de fechas', '4': 'Regresar al menú principal'}
-menuCalcularTotalGastos = {'1': 'Calcular total diario', '2': 'Calcular total semanal', '3': 'Calcular total mensual', '4': 'Regresar al menú principal'}
-menuGenerarReporte = {'1': 'Reporte diario', '2': 'Reporte semanal', '3': 'Reporte mensual', '4': 'Regresar al menú principal'}
