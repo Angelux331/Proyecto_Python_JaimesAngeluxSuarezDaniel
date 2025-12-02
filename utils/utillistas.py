d73bf102d67d2cc1b3ae15972de1aee68bceb123
Angelux331 <188609074+Angelux331@users.noreply.github.com>
2025-12-02 12:29:35 -0500

    Delete utils/utillistas.py

--- utils/utillistas.py
+++
@@ -1,8 +0,0 @@
-def findDictionary(dataList, key, value):
-    info = {}
-    for i in range(len(dataList)):
-        if dataList[i].get(key) == value:
-            info["index"] = i
-            info["data"] = dataList[i]
-            break
-    return info
